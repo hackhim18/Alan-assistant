b57cebaf42941c3199f5f2daf91b2de7d08ef921
Himanshu Tripathi <65823429+hackhim18@users.noreply.github.com>
2021-04-02 12:04:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React, { useState, useEffect } from 'react';
 import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-
-
 import NewsCards from './components/NewsCards/NewsCards'
 import useStyles from './styles';
  
